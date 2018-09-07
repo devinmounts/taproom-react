6cb3a2d57e05fbf33b8a9caab29e67f2f67e6b0d
Devin Mounts <devinmounts@gmail.com>
2018-09-07 12:47:56 -0700

    create callback handler in App Root

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,19 +6,84 @@ import KegList from './KegList';
 import NewKegControl from './NewKegControl';
 import Error404 from './Error404';
 
-function App(){
+class App extends React.Component(){
 
+  constructor(props){
+    super(props);
+    this.state = {
+      masterKegList: [
+        {
+          name: 'Ruby Zozzle',
+          brewer: 'Hi-Wheel',
+          description: 'Sparkling Wine & Grapefruit',
+          abv: '6.8%',
+          price: '7',
+          remaining: '20'
+        },
+        {
+          name: 'Tart N Juicy',
+          brewer: 'Epic',
+          description: 'Sour IPA',
+          abv: '4.5%',
+          price: '6',
+          remaining: '60'
+        },
+        {
+          name: 'Hamm\'s',
+          brewer: 'Miller/Coors',
+          description: 'American Lager',
+          abv: '4.7%',
+          price: '3',
+          remaining: '65'
+        },
+        {
+          name: 'Prismatic',
+          brewer: 'Ninkasi',
+          description: 'Juicy IPA',
+          abv:  '5.9%',
+          price: '6',
+          remaining: '75'
+        },
+        {
+          name: 'Juicy Haze',
+          brewer: 'New Belgium',
+          description: 'India Pale Ale',
+          abv:  '7.5%',
+          price: '6',
+          remaining: '18'
+        },
+        {
+          name: '8 Hop',
+          brewer: 'New Belgium',
+          description: 'Pale Ale',
+          abv:  '5.5%',
+          price: '6',
+          remaining: '58'
+        }
+      ]
+    };
+    this.handleAddingNewKegToList = this.handleAddingNewKegToList.bind(this);
+  }
+
+  handleAddingNewKegToList(newKeg){
+    let newMasterKegList = this.state.masterKegList.slice();
+    newMasterKegList.push(newKeg);
+    this.setState({masterKegList: newMasterKegList});
+  }
+
+  render(){
     return (
         <div>
-            <Nav/>
-            <Switch>
-              <Route exact path='/' component={Welcome} />
-              <Route path='/keglist' component={KegList} />
-              <Route path='/admin' component={NewKegControl} />
-              <Route component={Error404} />
-            </Switch>
+          <Nav/>
+          <Switch>
+            <Route exact path='/' component={Welcome} />
+            <Route path='/keglist' component={KegList} />
+            <Route path='/admin' component={NewKegControl} />
+            <Route component={Error404} />
+          </Switch>
         </div>
     );
+  }
 }
 
 export default App;
