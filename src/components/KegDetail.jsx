5279c168d0ce2e3cc5345799a478ef20592a25a5
Devin Mounts <devinmounts@gmail.com>
2018-09-14 08:54:55 -0700

    prepare to conditionally render admin details.

--- src/components/KegDetail.jsx
+++ src/components/KegDetail.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function KegDetail(){
+function KegDetail(props){
   return(
-
+    <div>
+      <h2>Admin Details</h2>
+    </div>
   );
 }
 
